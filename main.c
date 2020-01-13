b7bc270a8d4a997f389a1dc6c93cd2bb8ac6e50a
Swann <swann.herrera@gmail.com>
2020-01-13 21:08:06 +0100

    First commit
    initialisation du projet

---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+Author: Joëlle Castelli, Noé Larrieu-Lacoste, Swann HERRERA
+Objectif: Ouvrir un fichier ppm le lire avec plusieur thread voir syllabus
+Date: 13/01/2020
+*/
+#include <stdio.h>
+
+int
+main (int argc, char** argv) {
+
+}
